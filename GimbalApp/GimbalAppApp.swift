4f7975cc2af4161904e17b84476aedb11350e31a
Mohammad Bijantium Sinatria <79687063+Bintaaaa@users.noreply.github.com>
2022-10-01 00:02:04 +0700

    Initial Commit

---
+++ GimbalApp/GimbalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GimbalAppApp.swift
+//  GimbalApp
+//
+//  Created by Bijantyum on 01/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GimbalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
